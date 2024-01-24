3fe79730d874dc20a269e3d56462d96a6d035399
1vnazarov <nazaroffvana.boeing@yandex.ru>
2024-01-24 23:36:47 +0300

    Правки

--- Game.cs
+++ Game.cs
@@ -10,6 +10,7 @@ namespace Labitint {
         private int playerY;
         private int score;
 
+        Random rand = new Random();
         public Game(int width, int height) {
             map = new string[height, width];
             GenerateMap();
@@ -27,23 +28,23 @@ namespace Labitint {
                     map[i, j] = (i == 0 || i == map.GetLength(0) - 1 || j == 0 || j == map.GetLength(1) - 1) ? BrickSymbol : " ";
                 }
             }
+            PlaceObjects(ScoreSymbol, rand.Next(5, 10));
+            PlaceObjects(BrickSymbol, rand.Next(5));
+        }
 
-            Random rand = new Random();
-            int numberOfDollars = rand.Next(5, 10);
-
-            for (int i = 0; i < numberOfDollars; i++) {
+        private void PlaceObjects(string symbol, int count) {
+            for (int i = 0; i < count; i++) {
                 int x, y;
                 do {
                     x = rand.Next(1, map.GetLength(0) - 1);
                     y = rand.Next(1, map.GetLength(1) - 1);
                 } while (map[x, y] != " ");
 
-                map[x, y] = ScoreSymbol;
+                map[x, y] = symbol;
             }
         }
 
         private void PlacePlayer() {
-            Random rand = new Random();
             int x, y;
             do {
                 x = rand.Next(1, map.GetLength(0) - 1);
